dd5f47ef5aa353ba9637c108ab870f18d395782b
Daniel Mäckelmann <6890706+n1kPLV@users.noreply.github.com>
2020-06-01 20:08:07 +0200

    initial commit

---
+++ Bercow_Bot/Modules/AudioModule.cs
@@ -0,0 +1,36 @@
+﻿using Bercow_Bot.Services;
+using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bercow_Bot.Modules
+{
+    public class AudioModule : ModuleBase<ICommandContext>
+    {
+        private readonly AudioService _service;
+
+        public AudioModule(AudioService service)
+        {
+            _service = service;
+        }
+
+        [Command("join", RunMode = RunMode.Async)]
+        public async Task JoinCmd(IVoiceChannel channel = null)
+        {
+            // Get the audio channel
+            channel = channel ?? (Context.User as IGuildUser)?.VoiceChannel;
+            if (channel == null)
+            {
+                await Context.Channel.SendMessageAsync("User must be in a voice channel, or a voice channel must be passed as an argument.");
+                return;
+            }
+
+            // For the next step with transmitting audio, you would want to pass this Audio Client in to a service.
+            
+            await _service.JoinAudio(Context.Guild, channel);
+        }
+    }
+}
---
+++ Bercow_Bot/Services/AudioService.cs
@@ -0,0 +1,40 @@
+﻿using Discord;
+using Discord.Audio;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bercow_Bot.Services
+{
+    public class AudioService
+    { 
+        private readonly ConcurrentDictionary<ulong, IAudioClient> _connectedChannels = new ConcurrentDictionary<ulong, IAudioClient>();
+        private readonly DiscordSocketClient _discord;
+
+        public AudioService(IServiceProvider services)
+        {
+            _discord = services.GetRequiredService<DiscordSocketClient>();
+        }
+
+        public async Task JoinAudio(IGuild guild, IVoiceChannel target)
+        {
+            var audioClient = await target.ConnectAsync();
+
+            audioClient.SpeakingUpdated  += AudioClient_SpeakingUpdated;
+
+            _connectedChannels.TryAdd(guild.Id, audioClient);
+        }
+
+        private Task AudioClient_SpeakingUpdated(ulong userID, bool speaking)
+        {
+            SocketUser user = _discord.GetUser(userID);
+            Console.WriteLine(user.Username + (speaking ? " started" : " stopped") + " speaking");
+            return Task.CompletedTask;
+        }
+    }
+}
